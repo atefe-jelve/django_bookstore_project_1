07e30644916359d6221318f30ea1263ab36a29d4
atefe jelve <a.jelve.69@gmail.com>
2022-09-16 12:57:26 +0430

    add detail teplate and create

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookListView
+from .views import BookListView, BookDetailView, BookCreateView
 
 urlpatterns = [
-    path("", BookListView.as_view(), name='books_list')
-
+    path("", BookListView.as_view(), name='books_list'),
+    path("<int:pk>/", BookDetailView.as_view(), name='book_detail'),
+    path("create/", BookCreateView.as_view(), name='book_create'),
 ]
--- books/views.py
+++ books/views.py
@@ -7,3 +7,13 @@ class BookListView(generic.ListView):
     model = Book
     template_name = 'books/books_list.html'
 
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    template_name = 'books/book_create.html'
+    fields = ['title', 'author', 'description', 'price']
