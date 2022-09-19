f648bf5f732bc5fe2bb7d2d2ee9f1c74b08f1283
atefe jelve <a.jelve.69@gmail.com>
2022-09-19 16:10:22 +0430

    render book comments in book views

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Book
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
 
 
 class BookListView(generic.ListView):
@@ -10,9 +11,14 @@ class BookListView(generic.ListView):
     template_name = 'books/books_list.html'
 
 
-class BookDetailView(generic.DetailView):
-    model = Book
-    template_name = 'books/book_detail.html'
+# class BookDetailView(generic.DetailView):
+#     model = Book
+#     template_name = 'books/book_detail.html'
+
+def book_detail_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book_comments = book.comments.all
+    return render(request, 'books/book_detail.html', context={'book': book, 'comments': book_comments})
 
 
 class BookCreateView(generic.CreateView):
