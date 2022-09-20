9e0144b704cff5956977175c2415887b62226c94
atefe jelve <a.jelve.69@gmail.com>
2022-09-20 16:42:14 +0430

    required to details and create views

--- books/views.py
+++ books/views.py
@@ -4,6 +4,10 @@ from .models import Book
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 from .forms import CommentForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
+
 
 class BookListView(generic.ListView):
     model = Book
@@ -15,6 +19,7 @@ class BookListView(generic.ListView):
 #     model = Book
 #     template_name = 'books/book_detail.html'
 
+@login_required
 def book_detail_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book_comments = book.comments.all
@@ -34,7 +39,7 @@ def book_detail_view(request, pk):
                   context={'book': book, 'comments': book_comments, 'comment_form': comment_form})
 
 
-class BookCreateView(generic.CreateView):
+class BookCreateView(LoginRequiredMixin,generic.CreateView):
     model = Book
     fields = ['title', 'author', 'description', 'price', 'cover']
     template_name = 'books/book_create.html'
